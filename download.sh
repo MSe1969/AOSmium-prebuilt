a39d1f59ae4f8f359b1b91ea77a1f314ac55b197
mse1969 <mse1969@posteo.de>
2025-01-26 20:25:15 +0100

    AOSmium prebuilt - initial commit v132.0.6834.79

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl --fail -o prebuilt/arm/webview.apk  https://codeberg.org/AXP-OS_releases/app_aosmium/releases/download/latest/webview32-signed.apk
+curl --fail -o prebuilt/arm64/webview.apk https://codeberg.org/AXP-OS_releases/app_aosmium/releases/download/latest/webview64-signed.apk
+
+
+
